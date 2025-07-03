15927f3454379e8895795af971a7910928c3bb72
Bang04 <eunkyeong_b@naver.com>
2025-07-03 14:40:15 +0900

    fix : drop menu 영역외 클릭시 닫기 추가

--- personal-finance-app/src/components/modal/PotDropModal.tsx
+++ personal-finance-app/src/components/modal/PotDropModal.tsx
@@ -1,14 +1,34 @@
+import { useEffect, useRef } from "react";
 
 export const PotDropModal = ({ handleEditOpen, closeModal, position, id }: any) => {
 
-	const handleBackdropClick = (e : React.MouseEvent<HTMLDivElement>) => {
-		if((e.target as HTMLElement).id === "modal-backdrop"){
-          closeModal();  
-		}
-    }
+	const dropMenuRef = useRef<HTMLDivElement>(null);
+
+
+	useEffect(() => {
+		const handleDocumentClick = (e: {target : any}) => {
+			//e.target 이 Ref div 영역 안이면 true , 아니면 false 
+			if(!dropMenuRef.current?.contains(e.target)) closeModal();
+		};
+		document.addEventListener("mousedown", handleDocumentClick);
+		return () => {
+			document.removeEventListener("mousedown", handleDocumentClick);
+		};
+	}, [closeModal]);
+
+
+
+	// const handleBackdropClick = (e : React.MouseEvent<HTMLDivElement>) => {
+	
+		
+	// 	if((e.target as HTMLElement).id !== "modal-backdrop"){
+	// 	  closeModal();  
+	// 	}
+	// }
 
 	return (
-		<div
+		<div id="modal-backdrop"
+			ref={dropMenuRef}
 			className="absolute z-50 flex items-center bg-opacity-100 shadow-md" 
 			style={{ top: position.top , left: position.left }}
 		>
