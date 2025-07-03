59e3424cf68280a364ac1d393a57019d0f93844e
Bang04 <eunkyeong_b@naver.com>
2025-07-03 14:46:14 +0900

    docs : 불필요한 주석 제거

--- personal-finance-app/src/components/modal/PotDropModal.tsx
+++ personal-finance-app/src/components/modal/PotDropModal.tsx
@@ -3,8 +3,6 @@ import { useEffect, useRef } from "react";
 export const PotDropModal = ({ handleEditOpen, closeModal, position, id }: any) => {
 
 	const dropMenuRef = useRef<HTMLDivElement>(null);
-
-
 	useEffect(() => {
 		const handleDocumentClick = (e: {target : any}) => {
 			//e.target 이 Ref div 영역 안이면 true , 아니면 false 
@@ -16,26 +14,15 @@ export const PotDropModal = ({ handleEditOpen, closeModal, position, id }: any) 
 		};
 	}, [closeModal]);
 
-
-
-	// const handleBackdropClick = (e : React.MouseEvent<HTMLDivElement>) => {
-	
-		
-	// 	if((e.target as HTMLElement).id !== "modal-backdrop"){
-	// 	  closeModal();  
-	// 	}
-	// }
-
 	return (
 		<div id="modal-backdrop"
 			ref={dropMenuRef}
 			className="absolute z-50 flex items-center bg-opacity-100 shadow-md" 
 			style={{ top: position.top , left: position.left }}
 		>
-			{/* <div id="modal-backdrop"   onClick={handleBackdropClick}  className="absolute inset-0 bg-black opacity-50"></div> */}
 			<div className="flex flex-col bg-white rounded-xl shadow-xl relative">
-				<button onClick={(e) => handleEditOpen("edit", id)}  className="py-3 px-6">Edit Pot</button>
-				<button onClick={(e) => handleEditOpen("delete", id)} className="py-3 px-6 text-red-600" >Delete Pot</button>
+				<button onClick={() => handleEditOpen("edit", id)}  className="py-3 px-6">Edit Pot</button>
+				<button onClick={() => handleEditOpen("delete", id)} className="py-3 px-6 text-red-600" >Delete Pot</button>
 			</div>
 		</div>
 	);
