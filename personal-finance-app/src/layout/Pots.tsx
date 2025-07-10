ab06e9878c2554f50b7b8618859ec99f9b8c606d
Bang04 <eunkyeong_b@naver.com>
2025-07-10 17:14:25 +0900

    style : 스타일 수정

--- personal-finance-app/src/layout/Pots.tsx
+++ personal-finance-app/src/layout/Pots.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 import { useSelector } from "react-redux";
-import { rootState } from "../store"
+import { Pot, rootState } from "../store"
 
 import { Card } from "../components/Card"
 import { PotAddModal } from "../components/modal/PotAddModal";
@@ -15,11 +15,20 @@ import dots from "/images/dots-three-thin.svg";
 
 export const Pots = () => {
     const pots = useSelector((state:rootState)=> state.potReducer);
-    
+
+    const [data , setData ] = useState<Pot[]>();
     const [ isOpen, setIsOpen ] = useState(false);
     const [ id , setId ] = useState<string>();
     const [ modalType , setModalType ] = useState("");
     const [ modalPosition, setModalPosition ] = useState({top: 0, left:0});
+
+    
+
+    useEffect(() =>{
+        console.log(pots);
+        setData(pots);
+        
+    },[pots]);
 
     const closeModal = () =>{
         setIsOpen(false);
@@ -52,51 +61,51 @@ export const Pots = () => {
     }
 
     return (
-        <div className="flex flex-col p-8 mx-auto my-auto">
-             <div className="flex flex-row flex-nowrap justify-between mb-8">
+        <div className="flex flex-col min-w-sm md:w-3xl lg:w-5xl mx-auto my-auto">
+             <div className="flex flex-row flex-nowrap justify-between items-center  mb-8">
                  <div className="flex mt-5 font-semibold text-4xl mb-8">Pots</div>
-                
                  <button onClick={(e) => handleInsertModal("insert", e)}  className="text-sm text-white  bg-black font-semibold py-4 px-4 rounded-lg">+Add New Pot</button>
             </div>
 
-            <div className="flex flex-wrap md:flex-row">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 {
-                    pots.length > 0 ? (
-                    pots.map((item, index) => (
+                    data && data.length > 0 ? (
+                    data.map((item, index) => (
                     
-                        <div key={index} className="flex w-full  md:w-1/2 ">
+                        <div key={index} className="flex">
                             <input type="hidden" name="id" value={item.id} />
-                            <Card  title="" link="">
-                                <div className="flex justify-between">
-                                    
+                              <div className="bg-white rounded-xl shadow-md p-4 hover:shadow-lg transition-shadow cursor-pointer w-full">
+                          
+                                <div className="flex justify-between mb-6">
                                     <div className="flex flex-row items-center">
                                         <div className={`w-3 h-3 rounded-full `} style={{ backgroundColor: item.theme }}></div>
-                                        <span className="">{item.name}</span>
+                                        <span className="font-semibold pl-3">{item.name}</span>
                                     </div>
                                     <div className="w-10" onClick={(e) => handleDropModal("drop", e,item.id)} ><img src={dots} alt="" /></div>
                                 </div>
                                 <div>
-                                    <div className="flex justify-between"> 
+                                    <div className="flex justify-between mb-4"> 
                                         <span className="text-gray-500">Total Saved</span>
-                                        <span>${item.total}</span>
+                                        <span className="text-3xl font-semibold">${(item.total).toFixed(2)}</span>
                                     </div>
-                                    <div>
+                                    <div className="mb-4">
                                         <div className="w-full h-3 bg-gray-100">
                                             <div  className={`h-3 `} style={{ 
                                                   width: `${(item.total / item.target) * 100}%`,
-                                                  backgroundColor: item.theme }}></div>
+                                                  backgroundColor: item.theme }}>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div  className="flex justify-between">
+                                    <div  className="flex justify-between mb-6">
                                         <span className="text-gray-500">{((item.total / item.target) * 100).toFixed(2)}%</span>
                                         <span className="text-gray-500">Target of ${item.target}</span>
                                     </div>
-                                    <div className="flex gap-[10px] justify-between">
+                                    <div className="flex gap-[10px] justify-between  mb-3">
                                         <button onClick={() => handleEditOpen("add", item.id)}      className="p-3 text-sm bg-[#F8F4F0] font-semibold flex-1 rounded-lg">+Add Money</button>
                                         <button onClick={() => handleEditOpen("withdraw", item.id)} className="p-3 text-sm bg-[#F8F4F0] font-semibold flex-1 rounded-lg">Withdraw</button>
                                     </div>
                                 </div>
-                            </Card>
+                            </div>
                         </div>
                     
                     ))
