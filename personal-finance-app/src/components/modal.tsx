6d7c94a6e6efa84130b989299ef603fcca5ea1bc
Bang04 <eunkyeong_b@naver.com>
2025-07-31 15:13:09 +0900

    feat : Button 컴포넌트 추가

--- personal-finance-app/src/components/modal.tsx
+++ personal-finance-app/src/components/modal.tsx
@@ -1,48 +1,57 @@
 import { PropsWithChildren } from "react";
 import close from "/images/icon-close-modal.svg";
-
-export type modalType = "ADD" | "EDIT" | "REMOVE" ;
+import { Button } from "./Button";
+export type modalType = "ADD" | "EDIT" | "REMOVE";
 
 export type modal = {
 	type: modalType,
-    isOpen: boolean
-    closeModal: ()=>void
-    title?: string
-    description?: string
-	buttons?: {name: string, type: string, handler?: ()=>void,  color: {text: string, background: string}}[]
+	isOpen: boolean
+	closeModal: () => void
+	title?: string
+	description?: string
+	buttons?: { name: string, type: string, handler?: () => void, color: { text: string, background: string } }[]
 	prop?: any
 } & PropsWithChildren
 
-export const Modal = ({ type, isOpen, closeModal, title, description, buttons, children }:modal) => {
-    
-    if(!isOpen) return null;
+export const Modal = ({ type, isOpen, closeModal, title, description, buttons, children }: modal) => {
 
-    return (
-        <div className="fixed inset-0 z-50 flex justify-center items-center bg-opacity-100">
-            <div className="absolute inset-0 bg-black opacity-50"></div>
-            <div className="bg-white p-6 rounded-xl shadow-xl max-w-sm w-full relative">
+	if (!isOpen) return null;
+
+	return (
+		<div className="fixed inset-0 z-50 flex justify-center items-center bg-opacity-100">
+			<div className="absolute inset-0 bg-black opacity-50"></div>
+			<div className="bg-white p-6 rounded-xl shadow-xl max-w-sm w-full relative">
 				<div className="flex justify-between pb-5">
-					<span className="text-2xl self-center font-semibold">{ title }</span>
+					<span className="text-2xl self-center font-semibold">{title}</span>
 					<button
-						onClick={(e:any)=> closeModal()}
+						onClick={(e: any) => closeModal()}
 						className="p-2 rounded hover:bg-gray-100">
-							<img src={close} />
+						<img src={close} />
 					</button>
 				</div>
 				<div className="pb-5">
-					<p className="text-sm text-gray-500">{ description }</p>
+					<p className="text-sm text-gray-500">{description}</p>
 				</div>
-                { children }
+				{children}
 				{
-					buttons?.map((button,_index)=> (
+					buttons?.map((button, _index) => (
 						type == button.type ?
-							<button type="button" 
-								onClick = {(e)=> button.handler?  button.handler() : closeModal()} 
-								className={button.color.background+" my-1 cursor-pointer text-xs w-full py-3 px-4 font-normal rounded-md focus:outline-none "+button.color.text}>{ button?.name  }</button>:<></>
+							<Button
+								type='modal'
+								name={button?.name}
+								bg_color={button.color.background}
+								text_color={button.color.text}
+								closeModal={closeModal}
+								handler={button.handler}
+							></Button>
+						: <></>
+
 					))
 				}
-            </div>
-        </div>
-    );
+			</div>
+		</div>
+	);
 
 };
+
+
