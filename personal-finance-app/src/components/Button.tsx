76ec59b2f3647f7ed6793e78e5bd68c2d80a4da4
Bang04 <eunkyeong_b@naver.com>
2025-07-31 15:16:38 +0900

    docs : 주석 제거

--- personal-finance-app/src/components/Button.tsx
+++ personal-finance-app/src/components/Button.tsx
@@ -3,49 +3,28 @@ type button = {
     type: string,
     closeModal: () => void, //모달 닫기
     handler?: () => void,   //이벤트 핸들러
-    disabled? : boolean,
-    pageNum? : number,      // 페이징 필요
+    disabled?: boolean,
+    pageNum?: number,      // 페이징 필요
     bg_color: string,       //버튼 배경색
     text_color: string,     //버튼 글자색
     name: string | number   //버튼명 or value 사용
 }
 
-export const Button = ({ type, handler, closeModal,disabled, pageNum, bg_color, text_color, name }: button) => {
-   
-  switch (type) {
+export const Button = ({ type, handler, closeModal, disabled, pageNum, bg_color, text_color, name }: button) => {
+
+    switch (type) {
         case "modal":
-            return <button type="button" className={bg_color +" py-3 px-4 my-1 text-xs w-full  font-normal rounded-md cursor-pointer focus:outline-none "+text_color} onClick={() => handler ? handler() : closeModal()}> {name} </button>;
-       
+            return <button type="button" className={bg_color + " py-3 px-4 my-1 text-xs w-full  font-normal rounded-md cursor-pointer focus:outline-none " + text_color} onClick={() => handler ? handler() : closeModal()}> {name} </button>;
+
         case "page":
-            return  <button type="button" disabled={disabled} className="button px-5 py-1 rounded-sm border-1 cursor-pointer hover:bg-black hover:text-white hover:opacity-50" 
-                onClick={()=>handler}>{name}</button>;
+            return <button type="button" disabled={disabled} className="button px-5 py-1 rounded-sm border-1 cursor-pointer hover:bg-black hover:text-white hover:opacity-50"
+                onClick={() => handler}>{name}</button>;
 
         case "pnum":
-            return   <button key={name} type="button" className={`hover:bg-black hover:text-white hover:opacity-50 cursor-pointer w-8 h-8 rounded-sm border-1 mx-1 ${pageNum == name ? "bg-black text-white":""}`}
-                onClick={()=>handler}> {name} </button>;
+            return <button key={name} type="button" className={`hover:bg-black hover:text-white hover:opacity-50 cursor-pointer w-8 h-8 rounded-sm border-1 mx-1 ${pageNum == name ? "bg-black text-white" : ""}`}
+                onClick={() => handler}> {name} </button>;
         default:
             return <button type="button" className="py-4 px-4  text-sm text-white font-semibold  bg-black rounded-lg"
                 onClick={() => handler} >{name}</button>;
     }
 }
-
-// export const Button = ({ btype, handler, closeModal,disabled, pageNum, bg_color, text_color, name }: button) => {
-//    const buttonType = btype.trim().toLowerCase();
-//     console.log( "$$$$ "+ typeof buttonType);
-
-//    switch (buttonType) {
-//         case 'modal':
-//             return <button type="button" className={bg_color + "py-3 px-4 my-1 text-xs w-full  font-normal rounded-md cursor-pointer focus:outline-none " + text_color} onClick={() => handler ? handler() : closeModal()}> {name} </button>;
-       
-//         case 'page':
-//             return  <button type="button" disabled={disabled} className="button px-5 py-1 rounded-sm border-1 cursor-pointer hover:bg-black hover:text-white hover:opacity-50" 
-//                 onClick={()=>handler}>{name}</button>;
-
-//         case 'pnum':
-//             return   <button key={name} type="button" className={`hover:bg-black hover:text-white hover:opacity-50 cursor-pointer w-8 h-8 rounded-sm border-1 mx-1 ${pageNum == name ? "bg-black text-white":""}`}
-//                 onClick={()=>handler}> {name} </button>;
-//         default:
-//             return <button type="button" className="py-4 px-4  text-sm text-white font-semibold  bg-black rounded-lg"
-//                 onClick={() => handler} >{name}</button>;
-//     }
-// }
